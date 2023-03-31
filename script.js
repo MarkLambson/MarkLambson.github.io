6b17a10cb22d67813bba391188d5be3310d893d4
MarkLambson <mj.lambson@yahoo.com>
2023-03-31 05:37:30 -0700

    profile page 1.0

---
+++ script.js
@@ -0,0 +1,11 @@
+function openTabA() {
+    window.open("https://github.com/MarkLambson", "_blank");
+}
+
+function openTabB() {
+    window.open("https://www.linkedin.com/in/mark-lambson-47ba61a4/", "_blank");
+}
+
+function openTabC() {
+    window.open("mailto:mj.lambson@yahoo.com", "_blank");
+}
